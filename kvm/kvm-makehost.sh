a74c762c23319fe25e9060d2f4a6998ff2d00d9e
Sebastian Barry <59349192+sebbarry@users.noreply.github.com>
2022-08-13 16:35:44 -0400

    Update kvm-makehost.sh

--- kvm/kvm-makehost.sh
+++ kvm/kvm-makehost.sh
@@ -10,9 +10,8 @@ function queryforOS()
 function makeHost()
 {
   # here we are assuming that we have a host .iso file somewhere accessible on the host.
-  virst-install --network bridge:virbr0 --name VMNAME \
-    --os-variant=YOURVARIANTNAME --ram=RAMVALUE# --vcpus=CPUCOUNT \ 
-    --disk path=/var/lib/libvirt/images/VMNAME- s.qcow2,format=qcow2,bus=virtio,size=5 \
-    --graphics none --location=/path/to/.iso \
+  virt-install --network bridge:virbr0 --name test --os-variant=rocky9.0 --ram=1536 --vcpus=1 \
+    --disk path=/var/lib/libvirt/images/test-os.qcow2,format=qcow2,bus=virtio,size=5 \ 
+    --graphics none --location=Rocky-9.0-x86_64-minimal.iso \
     --extra-args="console=tty0 console=ttyS0,115200" --check all=off
 }
